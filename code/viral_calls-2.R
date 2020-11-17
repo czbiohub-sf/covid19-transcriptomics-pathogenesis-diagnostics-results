eaed4eed1f43c894e07f6ee0acfdc307f011497c
eranmick <52669207+eranmick@users.noreply.github.com>
2020-11-16 17:54:27 -0800

    Add files via upload

--- code/viral_calls-2.R
+++ code/viral_calls-2.R
@@ -13,9 +13,9 @@ resp_viruses %>%
   tibble::add_row(Pathogen = "Wuhan seafood market pneumonia virus") ->
   resp_viruses
 
-metatable <- read.csv(metatable_csv)
+metatable <- read.csv(metatable_csv, stringsAsFactors = F)
 
-viral_calls <- read.csv(viral_calls_csv, header = TRUE)
+viral_calls <- read.csv(viral_calls_csv, header = TRUE, stringsAsFactors = F)
 
 viral_calls %>%
   dplyr::mutate(sample_name=as.character(sample_name)) %>%
@@ -36,5 +36,24 @@ metatable %>%
       SC2_PCR=="POS" ~ "SC2",
       idseq_sample_name %in% dplyr::filter(
         viral_calls, !startsWith(name, "Wuhan"))$sample_name ~ "other_virus",
-      TRUE ~ "no_virus")) %>%
+      TRUE ~ "no_virus")) ->
+  metatable
+
+# Calculate mean age in each viral status group
+metatable %>% 
+  dplyr::group_by(viral_status) %>% 
+  dplyr::summarize("mean_age" = mean(age, na.rm = T)) %>% 
+  tibble::column_to_rownames("viral_status") ->
+  mean_age_by_viral_status
+
+# Set the age of patients with missing age to the mean age in their viral status group
+metatable %>% 
+  dplyr::rowwise() %>% 
+  dplyr::mutate("age" = ifelse(is.na(age),
+                        mean_age_by_viral_status[viral_status, "mean_age"],
+                        age)) %>% 
+  dplyr::ungroup() ->
+  metatable
+
+metatable %>%
   write.csv(out_table_csv, row.names=F, quote=F)
